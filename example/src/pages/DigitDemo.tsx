6274087fe4600b49d8a99acde5728c2db5c37f21
Kyle Soeltz <wsoeltz@gmail.com>
2020-09-24 12:13:39 -0400

    Update treemap digit demo to use more relevant data

--- example/src/pages/DigitDemo.tsx
+++ example/src/pages/DigitDemo.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import raw from 'raw.macro';
-import TreeMap, {transformData} from 'react-canvas-treemap';
+import TreeMap, {transformData, Output as TransformedData} from 'react-canvas-treemap';
 import styled from 'styled-components/macro';
 
 const Container = styled.div`
@@ -43,24 +43,8 @@ const colorMap = [
   { id: '8', color: '#F28188' },
 ]
 
-interface RawDatum {
-  id: number,
-  city_id: number,
-  year: number,
-  naics_id: number,
-  num_employ: number,
-  num_company: number,
-  level: number,
-}
-
-const rawData: RawDatum[][]  = [
-  JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_1.json')),
-  JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_2.json')),
-  JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_3.json')),
-  JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_4.json')),
-  JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_5.json')),
-  JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_6.json')),
-]
+const width = 800;
+const height = 600;
 
 interface PreparedDatum {
   id: string,
@@ -69,41 +53,125 @@ interface PreparedDatum {
   topLevelParentId: string,
 }
 
-const data: PreparedDatum[][] = rawData.map(d => d.map(({naics_id, num_company}) => {
-  let topLevelParentId: string = naics_id.toString();
-  let current: NaicsDatum | undefined = naicsData.find(datum => datum.naics_id === naics_id);
-  const title = current && current.name ? current.name : 'Unknown';
-  while(current && current.parent_id !== null) {
-    // eslint-disable-next-line
-    current = naicsData.find(datum => datum.naics_id === (current as NaicsDatum).parent_id);
-    if (current && current.parent_id !== null) {
-      topLevelParentId = current.parent_id.toString();
-    } else if (current && current.naics_id !== null) {
-      topLevelParentId = current.naics_id.toString();
+////////////////////////////////
+// interface RawDatum {
+//   id: number,
+//   city_id: number,
+//   year: number,
+//   naics_id: number,
+//   num_employ: number,
+//   num_company: number,
+//   level: number,
+// }
+// const rawData: RawDatum[][]  = [
+//   JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_1.json')),
+//   JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_2.json')),
+//   JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_3.json')),
+//   JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_4.json')),
+//   JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_5.json')),
+//   JSON.parse(raw('../data/boston_naics_sample/boston_sample_naics_6.json')),
+// ]
+
+// const data: PreparedDatum[][] = rawData.map(d => d.map(({naics_id, num_company}) => {
+//   let topLevelParentId: string = naics_id.toString();
+//   let current: NaicsDatum | undefined = naicsData.find(datum => datum.naics_id === naics_id);
+//   const title = current && current.name ? current.name : 'Unknown';
+//   while(current && current.parent_id !== null) {
+//     // eslint-disable-next-line
+//     current = naicsData.find(datum => datum.naics_id === (current as NaicsDatum).parent_id);
+//     if (current && current.parent_id !== null) {
+//       topLevelParentId = current.parent_id.toString();
+//     } else if (current && current.naics_id !== null) {
+//       topLevelParentId = current.naics_id.toString();
+//      }
+//   }
+//   if (parseInt(topLevelParentId, 10) > 8) {
+//     console.error(current);
+//     throw new Error('Parent out of range')
+//   }
+
+//   return {
+//     id: naics_id.toString(),
+//     title,
+//     value: num_company,
+//     topLevelParentId,
+//   }
+// }));
+
+// const transformedData: TransformedData[] = data.map(d => transformData({
+//   data: d,
+//   width,
+//   height,
+//   colorMap: colorMap,
+// }))
+//////////////////////////////////////
+
+
+interface RawDatum {
+  city_id: 945 | 1022,
+  name: "New York" | "Boston",
+  naics_id: number,
+  level: 1 | 2 | 3 | 4 | 5 | 6,
+  year: 2019 | 2020,
+  num_company: number,
+  num_employ: number,
+}
+
+const rawData: RawDatum[] = JSON.parse(raw('../data/bos_nyc_extract.json'));
+
+interface DataMap {
+  [key: number]: PreparedDatum[]
+}
+
+const data: DataMap = {
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+}
+rawData.forEach(({naics_id, num_company, level, city_id, year}) => {
+  if (city_id === 1022 && year === 2020) {
+    let topLevelParentId: string = naics_id.toString();
+    let current: NaicsDatum | undefined = naicsData.find(datum => datum.naics_id === naics_id);
+    const title = current && current.name ? current.name : 'Unknown';
+    while(current && current.parent_id !== null) {
+      // eslint-disable-next-line
+      current = naicsData.find(datum => datum.naics_id === (current as NaicsDatum).parent_id);
+      if (current && current.parent_id !== null) {
+        topLevelParentId = current.parent_id.toString();
+      } else if (current && current.naics_id !== null) {
+        topLevelParentId = current.naics_id.toString();
+      }
     }
-  }
-  if (parseInt(topLevelParentId, 10) > 8) {
-    console.error(current);
-    throw new Error('Parent out of range')
-  }
+    if (parseInt(topLevelParentId, 10) > 8) {
+      console.error(current);
+      throw new Error('Parent out of range')
+    }
 
-  return {
-    id: naics_id.toString(),
-    title,
-    value: num_company,
-    topLevelParentId,
+    data[level].push({
+      id: naics_id.toString() + level,
+      title,
+      value: num_company,
+      topLevelParentId,
+    })
   }
-}));
+});
 
-const width = 800;
-const height = 600;
+const transformedData: TransformedData[] = [];
 
-const transformedData = data.map(d => transformData({
-  data: d,
-  width,
-  height,
-  colorMap: colorMap,
-}))
+for (let d in data) {
+  transformedData.push(transformData({
+    data: data[d],
+    width,
+    height,
+    colorMap: colorMap,
+  }))
+}
+
+//////////////////////////////////////
+
 
 enum NumCellsTier {
   // Use `Small` for all type of tree maps except for product tree maps at
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,10 @@ import * as React from 'react'
 import Chart, {IProps} from './chart/chart';
 import treemapCellTransformer, {Inputs, Output} from './transformers/treemapCellTransformer';
 
-export const transformData: (inputs: Inputs) => Output = treemapCellTransformer;
+const transformData: (inputs: Inputs) => Output = treemapCellTransformer;
+export {
+  transformData, Inputs, Output,
+}
 
 export default (props: IProps) => {
   return (
